b7e3aa8cfcd236002f057b6aec177d99083fd71c
Dan Nguyen <dansonguyen@gmail.com>
2012-11-14 12:11:23 -0500

    del test

--- test/models/airline_delay_report_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
-
-describe "AirlineDelayReport Model" do
-  it 'can construct a new instance' do
-    @airline_delay_report = AirlineDelayReport.new
-    refute_nil @airline_delay_report
-  end
-end
